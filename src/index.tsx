bbd308c8324001a8c7e75b566bb06d065b7afb3d
Riley Bracken <rileybracken@gmail.com>
2019-01-18 10:29:06 -0700

    Switch to use Reacts context API, and adds TypeScript

---
+++ src/index.tsx
@@ -0,0 +1,106 @@
+import * as React from "react";
+
+declare global {
+  interface Window {
+    google_optimize?: {
+      get: (id: string) => string;
+    };
+    dataLayer: {
+      hide: {
+        end: () => void;
+      };
+    };
+    gtag: (
+      event: string,
+      callbackName: string,
+      callback: {
+        name: string;
+        callback: (value: string | undefined | null) => void;
+        remove?: boolean;
+      }
+    ) => void;
+  }
+}
+
+type Props = {
+  id?: string;
+  loader?: JSX.Element;
+};
+
+type State = Readonly<{
+  variant?: string;
+}>;
+
+const OptimizeContext = React.createContext<string | null>(null);
+
+class Experiment extends React.Component<Props, State> {
+  static defaultProps: Props = {
+    loader: null,
+    id: null
+  };
+
+  state: State = {
+    variant: null
+  };
+
+  updateVariant = (value: string | undefined | null) => {
+    this.setState(() => ({
+      variant: value === undefined || value === null ? "0" : value
+    }));
+  };
+
+  delayedInitialization = () => {
+    const value =
+      window.google_optimize && window.google_optimize.get(this.props.id);
+    this.updateVariant(value);
+  };
+
+  componentDidMount() {
+    if (!this.props.id) {
+      throw new Error("Please specify the experiment id");
+    }
+
+    // Delayed init
+    const hideEnd =
+      window.dataLayer && window.dataLayer.hide && window.dataLayer.hide.end;
+    if (hideEnd) {
+      window.dataLayer.hide.end = () => {
+        this.delayedInitialization();
+        hideEnd();
+      };
+    } else {
+      this.delayedInitialization();
+    }
+
+    window.gtag &&
+      window.gtag("event", "optimize.callback", {
+        name: this.props.id,
+        callback: this.updateVariant
+      });
+  }
+
+  componentWillUnmount() {
+    window.gtag &&
+      window.gtag("event", "optimize.callback", {
+        name: this.props.id,
+        callback: this.updateVariant,
+        remove: true
+      });
+  }
+
+  render() {
+    return (
+      <OptimizeContext.Provider value={this.state.variant}>
+        {this.state.variant === null ? this.props.loader : this.props.children}
+      </OptimizeContext.Provider>
+    );
+  }
+}
+
+const Variant = () => (
+  <OptimizeContext.Consumer>
+    {value => (value === this.props.id ? this.props.children : null)}
+  </OptimizeContext.Consumer>
+);
+
+export { Experiment, Variant };
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = env => ({
   mode: env.production ? "production" : "development",
-  entry: path.resolve(__dirname, "src/index.js"),
+  entry: path.resolve(__dirname, "src/index.tsx"),
   output: {
     path: path.resolve(__dirname, "lib"),
     filename: "react-optimize.js",
@@ -11,18 +11,31 @@ module.exports = env => ({
     umdNamedDefine: true
   },
   externals: {
-    react: "react",
-    "prop-types": "prop-types"
+    react: "react"
   },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  devtool: "inline-source-map",
   module: {
     rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: "awesome-typescript-loader",
+          options: {
+            errorsAsWarnings: true
+          }
+        },
+        exclude: /node_modules/
+      },
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
+            presets: ["@babel/preset-react"],
             plugins: ["@babel/plugin-proposal-class-properties"]
           }
         }
